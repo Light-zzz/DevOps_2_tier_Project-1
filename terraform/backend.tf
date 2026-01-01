7ec819d4d9dba3620c0c38266d86dab17d88bbae
Dipesh Vishwakarma <dipeshvishwa767@gmail.com>
2026-01-01 17:38:04 +0530

    Delete terraform directory

--- terraform/backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-required_version = ">= 1.0"
-
- backend "s3" {
-  bucket = "dipeshdevops4stroage" # CHANGE
-  key = "DevOpsProjectTerraform-1/pythonapplication.tfstate" # CHANGE
-  region = "eu-north-1" # CHANGE
- }
-}
