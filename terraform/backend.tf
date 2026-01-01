f96b11c2834ac5fcf8d64dbd2054bf5e41f5fc9f
Dipesh Vishwakarma <dipeshvishwa767@gmail.com>
2026-01-01 12:02:34 +0530

    Update backend.tf

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,9 +1,9 @@
 terraform {
 required_version = ">= 1.0"
 
-# backend "s3" {
-#  bucket = "dipeshbucket4practice" # CHANGE
-#  key = "production4practice/Jenkins.tfstate" # CHANGE
-#  region = var.region # CHANGE
-# }
+ backend "s3" {
+  bucket = "dipeshdevops4stroage" # CHANGE
+  key = "DevOpsProjectTerraform-1/pythonapplication.tfstate" # CHANGE
+  region = var.region # CHANGE
  }
+}
